97a2b50404e284e05ce9c674c43ff5e819891b07
Mouad Benchchaoui <mouad.benchchaoui@deliveryhero.com>
2015-01-09 11:41:07 +0100

    First work !

--- lymph/events/base.py
+++ lymph/events/base.py
@@ -1,9 +1,19 @@
 from abc import ABCMeta, abstractmethod
+import logging
+import sys
+
 import six
+
+
+logger = logging.getLogger(__name__)
 
 
 @six.add_metaclass(ABCMeta)
 class BaseEventSystem(object):
+
+    def __init__(self, container):
+        self.container = container
+
     @classmethod
     def from_config(cls, config, **kwargs):
         return cls(**kwargs)
@@ -26,3 +36,21 @@ class BaseEventSystem(object):
     @abstractmethod
     def emit(self, container, event):
         raise NotImplementedError
+
+
+class MessageHandler(object):
+
+    def __init__(self, event_system):
+        self.event_system = event_system
+
+    def on_message(self, body, message):
+        try:
+            self.handle_message(body, message)
+        except:
+            self.on_error(sys.exc_info())
+
+    def on_error(self, exc_info):
+        self.event_system.container.error_hook(exc_info)
+
+    def handle_message(self, body, message):
+        pass
