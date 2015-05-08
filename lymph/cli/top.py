b4d69a559855c592c6c58546b81d9dad48ea188f
mouad.benchchaoui@deliveryhero.com <mouadino@gmail.com>
2015-05-08 15:55:46 +0200

    clean up stuff.

--- lymph/cli/top.py
+++ lymph/cli/top.py
@@ -1,6 +1,9 @@
 # -*- coding: utf-8 -*-
 from __future__ import print_function
+import sys
 import time
+import collections
+import traceback
 
 from blessings import Terminal
 
@@ -8,42 +11,42 @@ from lymph.client import Client
 from lymph.cli.base import Command
 
 
-class Metrics(object):
-    mapping = (('InterfaceName', 'interface_name'), ('Endpoint', 'endpoint'), ("GreenLCount", 'greenlets.count'), ('GeventActive', 'gevent.active'))
-
-    def __init__(self, metrics):
-        self.metrics = metrics
-
-    def get(self):
-        result = []
-        for metric in self.metrics:
-            new_metrics = {}
-            for new, old in self.mapping:
-                new_metrics[new] = metric[old]
-            result.append(new_metrics)
-        return result
-
-
 class Table(object):
 
-    def __init__(self, *headers):
+    def __init__(self, headers):
         self.headers = headers
-        self.rows = []
-
-    def _truncate(self, name, size):
-        name = str(name)
-        if len(name) > size:
-            return name[:size-2] + ".."
-        return name.ljust(size)
+        self.instances = []
 
     def display(self, terminal):
-        for header, size in self.headers:
+        for header, size in self.headers.items():
             header = self._truncate(header, size)
             print(terminal.bold(header), end=' ')
-        print() # flush
-        for columns in self.rows:
-            print(' '.join([self._truncate(columns[name], size) for name, size in self.headers]))
-        print() # flush
+        print()  # flush
+        for instance in self.instances:
+            print(' '.join(self._truncate(getattr(instance, name), size) for name, size in self.headers.items()))
+        print()  # flush
+
+    @staticmethod
+    def _truncate(name, size):
+        name = str(name)
+        if len(name) > size:
+            return name[:size - 2] + ".."
+        return name.ljust(size)
+
+
+class InstanceInfo(collections.namedtuple('InstanceInfo', 'name endpoint metrics')):
+
+    def __getattr__(self, name):
+        return self.metrics[name]
+
+
+class Debugger(object):
+    def __enter__(self):
+        return self
+
+    def __exit__(self, type, value, tb):
+        if type:
+            traceback.print_exception(type, value, tb, file=sys.stdout)
 
 
 class TopCommand(Command):
@@ -58,30 +61,36 @@ class TopCommand(Command):
 
     short_description = 'Show services metrics.'
 
-    def get_instrance_metrics(self, instance):
-        metrics = self.client.request(instance.endpoint, 'lymph.get_metrics', {}).body
-        return {metric[0]: metric[1] for metric in metrics}
+    def run(self):
+        with Debugger():
+            terminal = Terminal()
+            table = Table({
+                'name': 15,
+                'endpoint': 25,
+                'greenlets.count': 20,
+                'gevent.active': 20
+            })
 
-    def get_metrics(self):
+            with terminal.fullscreen():
+                while True:
+                    table.instances = self.get_instances()
+                    table.display(terminal)
+                    time.sleep(1)
+                    print(terminal.clear, end='')
+
+    def get_instances(self):
         self.client = Client.from_config(self.config)
         services = self.client.container.discover()
-        metrics = []
-        if services:
-            for interface_name in sorted(services):
-                interface_instances = self.client.container.lookup(interface_name)
-                for instance in interface_instances:
-                    metrics.append({"interface_name": interface_name, "endpoint": instance.endpoint})
-                    metrics[-1].update(self.get_instrance_metrics(instance))
-        return metrics
+        instances = []
+        for interface_name in sorted(services):
+            interface_instances = self.client.container.lookup(interface_name)
+            for instance in interface_instances:
+                metrics = self.get_instance_metrics(instance)
+                instances.append(
+                    InstanceInfo(interface_name, instance.endpoint, metrics)
+                )
+        return instances
 
-    def run(self):
-        terminal = Terminal()
-        table = Table(('InterfaceName', 15), ('Endpoint', 25), ("GreenLCount", 20), ('GeventActive', 20))
-
-        with terminal.fullscreen():
-            while True:
-                metrics = self.get_metrics()
-                table.rows = Metrics(metrics).get()
-                table.display(terminal)
-                time.sleep(1)
-                print(terminal.clear, end='')
+    def get_instance_metrics(self, instance):
+        metrics = self.client.request(instance.endpoint, 'lymph.get_metrics', {}).body
+        return {name: value for name, value, _ in metrics}
